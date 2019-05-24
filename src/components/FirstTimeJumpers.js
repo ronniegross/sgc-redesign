76a5af21c361f65c5707b17909680e4eb03217f8
Ronnie Gross <rnngrss1495@gmail.co>
2019-05-23 23:40:36 -0400

    fixed off center first time jumper mobile modal issue

--- src/components/FirstTimeJumpers.js
+++ src/components/FirstTimeJumpers.js
@@ -31,11 +31,11 @@ const Wrapper = styled.div`
             flex-direction: column;
         }
     }
-    @media (max-width: 400px) {
+    /* @media (max-width: 400px) {
         .first-time-skydive-container {
             width: 90%;
         }
-    }
+    } */
 `
 const TandemData = {
     title: "Tandem Skydive",
